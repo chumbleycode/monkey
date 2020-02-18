5623903b1e824d37b8e63b87e92472544a61a701
Justin Chumbley <justin.chumbley@pm.me>
2020-02-18 10:44:02 +0100

    new data

--- docker_run.sh
+++ docker_run.sh
@@ -5,4 +5,4 @@ docker run -it \
      -p 8787:8787  -v /Volumes/Share/projects/monkey:/home/rstudio/workspace \
      -v /Volumes/Share/preprocessed:/home/rstudio/workspace/data/preprocessed \
      -v /Volumes/share/projects/cell_type_cibersort:/home/rstudio/workspace/data/cell_type_cybersort \
-     chumbleycode/monkey:0.0.19  bash
+     chumbley/monkey:0.0.1 bash
